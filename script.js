f07084bb21d40c5bb6e211058faf9a6c83f533a6
Lionel Florentin <lioneldrm19@gmail.com>
2026-03-17 10:23:57 -0300

    feat: modificación del token api

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ pedido.addEventListener("submit", (e) => {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer TEST-tu-token"
+            "Authorization": "Bearer TEST-APP_USR-583756990100420-031208-d69de8e4a45e6dc08a239304c9fbb038-3260786029"
         },
         body: JSON.stringify({
             items: items,

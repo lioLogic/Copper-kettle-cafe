9b1ab55e0ba22f631e3beeff3cb6311a2db2a331
Lionel Florentin <lioneldrm19@gmail.com>
2026-03-18 08:10:37 -0300

    fix: probar console.log en el featch

--- script.js
+++ script.js
@@ -131,6 +131,7 @@ if (pedido) {
     .then(response =>
         response.json())
     .then(data =>{
+        console.log(data);
         window.location.href = data.init_point
         limpiarFormulario();
     })
